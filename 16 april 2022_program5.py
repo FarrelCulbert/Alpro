802ba9ec8fc4c044ebcae4d9025fbb6ae2c10f67
FarrelCulbert <101645434+FarrelCulbert@users.noreply.github.com>
2022-04-16 21:37:20 +0700

    pengulangan
    segitiga setengah

---
+++ 16 april 2022_program5.py
@@ -0,0 +1,10 @@
+angka_awal = 2
+jarak_permulaan = 1
+baris = 7
+ 
+for i in range(baris):
+    for column in range(1, jarak_permulaan+1):
+        print(angka_awal, end=' ')
+        angka_awal += 1
+    print("")
+    jarak_permulaan += 1
