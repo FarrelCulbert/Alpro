61c1dd42e5dd33a2d536ba53d0f0a6b8e433776a
FarrelCulbert <101645434+FarrelCulbert@users.noreply.github.com>
2022-04-17 12:38:53 +0700

    Update 16 april 2022_program5.py

--- 16 april 2022_program5.py
+++ 16 april 2022_program5.py
@@ -2,7 +2,7 @@ angka_awal = 2
 jarak_permulaan = 1
 baris = 7
  
-for i in range(baris)
+for i in range(baris):
     for column in range(1, jarak_permulaan+1):
         print(angka_awal, end=' ')
         angka_awal += 1
